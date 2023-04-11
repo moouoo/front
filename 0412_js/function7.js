669c6a3508bc6a5ca32519923fb615f976db4fba
moouoo <0109963@naver.com>
2023-04-11 17:38:29 +0900

    20230411

---
+++ 0412_js/function7.js
@@ -0,0 +1,10 @@
+function atom3() {
+    document.write("이곳은 atom3입니다" + (100+300) + "<br/>");
+    console.log("atom3입니다.", + (100+300));
+    alert("이곳은 atom3입니다." + 100);
+};
+function atom4(mbc) {
+    document.write("이곳은 atom4입니다" + (mbc+300) + "<br/>");
+    console.log("atom4입니다.", + (mbc+300));
+    alert("이곳은 atom4입니다." + (mbc+400));
+};
